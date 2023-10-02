5b5b420a633c746a1771b9b6fd7553ab43632224
soham0209 <soham.juetce@gmail.com>
2023-10-01 21:21:22 -0400

    Added functionality in test_fzz.py

--- test_fzz.py
+++ test_fzz.py
@@ -5,7 +5,12 @@ if __name__ == '__main__':
     fzz = pyfzz()
     data = fzz.read_file('sample_filt')
     bars = fzz.compute_zigzag(data)
+    print(f'Bars from file {bars}')
+    # Write the result into a file.
+    
     fzz.write_file('sample_filt_pers', bars)
-    # bars = fzz.compute_zigzag([('i', [0]), ('i', [1]), ('i', [0, 1])])
 
+    # You can use the following code to test the function directly with a list of insertion and deletion operations.
+    
+    bars = fzz.compute_zigzag([('i', [0]), ('i', [1]), ('i', [0, 1])])
     print(bars)
